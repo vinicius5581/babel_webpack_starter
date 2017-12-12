5a1accb20166bd526ec575f040cc209e3dbd2eb5
Vinicius Santana <santanaviny@gmail.com>
2017-12-11 19:11:24 -0800

    Adding Jquery

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
-import { person, sayHello } from './lib';
+import { person, sayHello, people } from './lib';
+
+let $ = require('jquery');
+$('body').append(`<h1>${person.name} </h1>`);
+
+$.each(people, (key, value) => $('body').append(`<h5>${people[key].name} </h5>`));
 
 console.log(person.name);
-
 console.log(sayHello('Vini'));
+console.log('people: ', people);
+console.log(require('./lib'));
 
 async function getPosts() {
   const response = await fetch('http://jsonplaceholder.typicode.com/posts');
--- src/lib.js
+++ src/lib.js
@@ -3,6 +3,12 @@ export const person = {
   age: 36
 }
 
+export const people = [
+  {name: 'Brodie B'},
+  {name: 'John D'},
+  {name: 'Nancy T'},
+];
+
 export function sayHello(name) {
   return `Hello ${name}`;
 }
